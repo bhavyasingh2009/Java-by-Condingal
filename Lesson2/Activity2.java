1cb99f75b339cb1a8a8e69c2647941555861f50c
bhavyasingh2009 <bs0809155@gmail.com>
2025-04-08 18:38:36 +0530

    Lesson 1 Activit 2

---
+++ Lesson2/Activity2.java
@@ -0,0 +1,14 @@
+package Lesson2;
+
+public class Activity2 {
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 54;
+        int sum = a + b;
+        System.out.println(sum); 
+        --sum;
+        System.out.println(sum); 
+        ++sum;
+        System.out.println(sum);
+    }
+}
