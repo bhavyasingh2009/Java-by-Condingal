082d671759bd710e933bcd84eeba9b8bb84ac1fe
bhavyasingh2009 <bs0809155@gmail.com>
2025-04-08 18:53:44 +0530

    Lesson 1 Project 1

---
+++ Lesson1/Project1.java
@@ -0,0 +1,9 @@
+public class Project1 {
+    public static void main(String[] args) {
+        int a = 58;
+        int b = 996;
+        int sum = a + b;
+        System.out.println(sum);
+
+    }
+}
