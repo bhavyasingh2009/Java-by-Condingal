04b637440a890c881ed7b1d3abf54588e9b3ccda
bhavyasingh2009 <bs0809155@gmail.com>
2025-04-08 18:29:41 +0530

    Lessson 2 Activity 1

---
+++ Lesson2/Calci.java
@@ -0,0 +1,10 @@
+package Lesson2;
+
+public class Calci {
+    public static void main (String [] args){
+        int a = 108;
+        int b = 52;
+        int sum = a + b;
+        System.out.println(sum );
+    }
+}
