bd3693ac554ed65ef35ab4d037ca09d10793442e
bhavyasingh2009 <bs0809155@gmail.com>
2025-04-08 18:48:32 +0530

    Lesson 2 Activity 3

---
+++ Lesson2/Activity3.java
@@ -0,0 +1,11 @@
+package Lesson2;
+
+public class Activity3 {
+    public static void main(String[] args) {
+        bhavya();
+    }
+    public static int bhavya () {
+        System.out.println("hi my name is codingal");
+        return 5;
+    }
+}
