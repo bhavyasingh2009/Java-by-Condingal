577656d9b333b16c6008d7f0794e8d62701649b9
bhavyasingh2009 <bs0809155@gmail.com>
2025-04-15 18:14:32 +0530

    Lesson 3 Project 1

---
+++ Lesson3/project1.java
@@ -0,0 +1,15 @@
+ package Lesson3;
+import java.util.Scanner;
+public class project1{
+    public static void main(String[] args){
+        Scanner ankush = new Scanner (System.in);
+        int x = ankush.nextInt();
+        if(x == 5){
+            System.out.println("true");
+        }
+else{
+    System.out.println("false");
+}
+        
+}
+} 
