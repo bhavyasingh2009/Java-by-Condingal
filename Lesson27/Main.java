4a1486a3984eaf613ec1f325921f316ae1e59338
bhavyasingh2009 <bs0809155@gmail.com>
2025-09-24 22:36:27 +0530

    lesson27activity1

---
+++ Lesson27/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        System.out.println("enter the " + n + " elements of the array: ");
+        for (int i = 0; i<n; i++){
+            arr[i] = scn.nextInt();
+        }
+        scn.close();
+        displayArr(arr,0);
+    }
+
+    public static void displayArr(int[] arr, int index){
+        if(index== arr.length){
+            return;
+        }
+        System.out.println("printin the element " + index + " of array which is " + arr[index]);
+        displayArr(arr, index+1);
+    }
+}`
